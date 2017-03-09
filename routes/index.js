6c39f4c64d40a373eb3f46f790b415055e182c27
Nick Stetich <nstetich@gmail.com>
2017-03-09 13:48:00 -0800

    Initial revision

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/roll', function(req, res) {
+  console.log(req.body);
+  res.status(200).json({
+    attachments: [
+      {
+        color: "good",
+        title: "@" + req.body.user_name + " rolled *100*",
+        fields: [
+          {
+            title: "Dice",
+            value: "1d100",
+            short: true
+          },
+          {
+            title: "Rolls",
+            value: "100",
+            short: true
+          }
+        ]
+      }
+    ]
+  });
+});
+
+module.exports = router;
